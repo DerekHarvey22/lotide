760a662424523ab07a300647fcb20f3d44b7099f
Derek Harvey <derek.harvey0422@gmail.com>
2022-03-10 19:04:44 +0000

    Implement count letters

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🆘🆘🆘 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const countLetters = function(string){
+  const letterCount = {};
+
+  for(let char of string){
+    if(char !== " "){
+      if(letterCount[char]){
+        letterCount[char] += 1;
+      }else{
+        letterCount[char] = 1;
+      }
+    }
+  }
+  return letterCount;
+};
+
