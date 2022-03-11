1d04b05f0a5e68ee986f1e9769c75ffccf9908ad
Derek Harvey <derek.harvey0422@gmail.com>
2022-03-11 21:19:22 +0000

    implement map

---
+++ map.js
@@ -0,0 +1,35 @@
+const assertArraysEqual = function(actual, expected) {
+  let conditionsMet;
+  if (conditionsMet === true) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🆘🆘🆘 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+}
+const eqArrays = function (actual, expected) {
+  let conditionsMet = true;
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        conditionsMet = false;
+      };
+      
+    }
+  } else {
+    conditionsMet = false;
+  } 
+  return conditionsMet;
+}
+
+const map = function(array, callback) {
+const results = [];
+for (let item of array) {
+results.push(callback(item));
+  }
+  return results;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0])
+console.log(results1);
