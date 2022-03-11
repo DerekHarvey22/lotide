37e18fa5795bf8d3f1e790693724ea4f185f2136
Derek Harvey <derek.harvey0422@gmail.com>
2022-03-11 21:57:21 +0000

    implement take until

---
+++ takeUntil.js
@@ -0,0 +1,45 @@
+const assertArraysEqual = function(actual, expected) {
+  let conditionsMet;
+  if (conditionsMet === true) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🆘🆘🆘 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+}
+const eqArrays = function (actual, expected) {
+  let conditionsMet = true;
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        conditionsMet = false;
+      };
+      
+    }
+  } else {
+    conditionsMet = false;
+  } 
+  return conditionsMet;
+}
+
+const takeUntil = function(array, callback) {
+  const elements = [];
+  for (const element of array) {
+    if(callback(element)) {
+      return elements;
+    } else {
+      elements.push(element);
+    }
+  }
+  return elements;
+}
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
