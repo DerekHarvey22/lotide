aa57004c6a02dbab9e2f4f5f573ab4a07bf22cad
Derek Harvey <derek.harvey0422@gmail.com>
2022-03-14 20:55:51 +0000

    added tests for head.js and assertEqual.js

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+// test/assertEqualTest.js
+
+const assertEqual = require('../assertEqual');
+
+// Test code:
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
---
+++ test/headTest.js
@@ -0,0 +1,7 @@
+//test/headTest.js
+const head = require('../head');
+
+
+//Test code
+console.log(head([5,6,7]), 5);
+console.log(head(["Hello", "Lighthouse", "Labs"]), "Hello");
