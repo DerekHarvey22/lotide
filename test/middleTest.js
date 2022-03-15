0842f03008380431e4218cf431dab0d643dbdd45
Derek Harvey <derek.harvey0422@gmail.com>
2022-03-15 00:50:23 +0000

    added chai and mocha to middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,15 @@
 // testing middle.js
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
+describe("#middle", () => {
+  it("returns [3, 4] for ['1', '2', '3', '4', '5', '6']", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  })
+})
 
 
 
 
-//test code
-console.log (assertArraysEqual(middle([1, 2, 3, 4, 5, 6]))) /// should fail and print 3,4 and undefined
+
